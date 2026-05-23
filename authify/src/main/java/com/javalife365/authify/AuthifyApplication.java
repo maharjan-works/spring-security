1b4b09835d6da44deba5f2471ef09c169faf10a5
javalife365 <java.life.365@gmail.com>
2026-05-23 08:15:02 -0400

    ui and api setup completed, api - user entity and its repo added

---
+++ authify/src/main/java/com/javalife365/authify/AuthifyApplication.java
@@ -0,0 +1,13 @@
+package com.javalife365.authify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthifyApplication.class, args);
+	}
+
+}
---
+++ authify/src/test/java/com/javalife365/authify/AuthifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalife365.authify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
